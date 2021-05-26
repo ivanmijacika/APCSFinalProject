e15e695984c6736c5873426f6f4407d3e1eeb539
yelsharawy <yelsharawy20@stuy.edu>
2021-05-26 18:13:00 -0400

    Created test class

---
+++ src/MyClass.java
@@ -0,0 +1,13 @@
+public class MyClass {
+
+    public MyClass() { }
+
+    public int getValue() {
+        return 5;
+    }
+
+    public static void helloWorld() {
+        System.out.println("Hello World!");
+    }
+
+}
