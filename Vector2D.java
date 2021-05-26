86b8220ede0a8f327b84dee95133afdf59e1208a
ivanmijacika <imijacika20@stuy.edu>
2021-05-26 19:21:08 -0400

    Vector2D constructor added

--- Vector2D.java
+++ Vector2D.java
@@ -3,4 +3,14 @@ public class Vector2D{
   private double x;
   private double y;
 
+  public Vector2D(double x, double y){
+    this.x = x;
+    this.y = y;
+  }
+
+//  public double getX(){}
+
+//  public double getY(){}
+
+//  public double magnitude(){}
 }
