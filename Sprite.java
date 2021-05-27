c0f0c99041343be64d11bb0095ab446504596a8c
ivanmijacika <imijacika20@stuy.edu>
2021-05-26 21:17:11 -0400

    private variables image and pivot added, and one of the three constructors was completed. The other two cannot yet be completed because we haven't completed Tile and Entity yet

---
+++ Sprite.java
@@ -0,0 +1,15 @@
+public class Sprite{
+
+  private int[][] image;
+  private Vector2D pivot;
+
+//  public Sprite(Tile tile){}
+
+//  public Sprite(Entity entity){}
+
+  public Sprite(int[][] image, Vector2D pivot){
+    this.image = image;
+    this.pivot = pivot;
+  }
+
+}
