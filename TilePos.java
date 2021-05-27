ee4e9d78a6467c7529edcd84455243eafd220f04
ivanmijacika <imijacika20@stuy.edu>
2021-05-26 21:00:13 -0400

    private variables x and y added, and first constructor (using int x and int y) completed

---
+++ TilePos.java
@@ -0,0 +1,16 @@
+public class TilePos{
+
+  private int x;
+  private int y;
+
+  public TilePos(int x, int y){
+    this.x = x;
+    this.y = y;
+  }
+/*
+  public TilePos(Vector2D p){
+    x = ;
+    y = ;
+  }
+*/
+}
