482c075f767a423987ff7b3d63dd0196be759131
yelsharawy <yelsharawy20@stuy.edu>
2021-05-27 12:42:04 -0400

    Added World class instance variables and constructor (fills with air)

---
+++ World.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class World {
+
+    private int width;
+    private int height;
+    private Tile[][] tiles;
+    private Set<Entity> entities;
+    /*
+    private Player player;
+    */
+    private Vector2D cameraPos;
+
+    public World(long seed) {
+        width = 100;
+        height = 100;
+        tiles = new Tile[height][width];
+        for (Tile[] row : tiles) {
+            for (int i = 0; i < row.length; i++) {
+                row[i] = Tile.AIR;
+            }
+        }
+    }
+    
+}
