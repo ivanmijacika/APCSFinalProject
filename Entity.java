7c8f3bce4309d2ad335b9f4e50ea5ecee2705eed
yelsharawy <yelsharawy20@stuy.edu>
2021-05-27 01:11:57 -0400

    Added Entity abstract class with constructor

---
+++ Entity.java
@@ -0,0 +1,11 @@
+public abstract class Entity {
+
+    private Vector2D position;
+    private Vector2D velocity;
+
+    public Entity(Vector2D pos, Vector2D vel) {
+        position = pos;
+        velocity = vel;
+    }
+    
+}
