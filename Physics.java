0fffed6f878594d1c0555f6bff43ac7e636de76b
yelsharawy <yelsharawy20@stuy.edu>
2021-06-03 03:30:29 -0400

    Tweaked physics constants & implemented (unfinished) player controls

--- Physics.java
+++ Physics.java
@@ -18,15 +18,16 @@ public class Physics {
 
     }
 
-    public static final Vector2D GRAVITY = new Vector2D(0, 9.81);
+    public static final Vector2D GRAVITY = new Vector2D(0, 50);
+    public static final double MAX_FALL = 50;
 
     public static MoveResult moveEntity(World world, Entity entity, double deltaSec) {
         //Vector2D pos = entity.getPosition();
         Vector2D vel = entity.getVelocity();
 
         Vector2D newV = vel.add(GRAVITY.multiply(deltaSec));
-        if (newV.getY() > 10) {  // maximum falling speed
-            newV = new Vector2D(newV.getX(), 10);
+        if (newV.getY() > MAX_FALL) {  // maximum falling speed
+            newV = new Vector2D(newV.getX(), MAX_FALL);
         }
         entity.setVelocity(newV);
 
--- Player.java
+++ Player.java
@@ -3,12 +3,14 @@ public class Player extends Entity {
     private World world;
     private ISprite sprite;
     private IInput input;
+    private View view;
 
     public Player(World world, Vector2D pos, Vector2D vel) {
         super(world, pos, vel, new Vector2D(1.5, 2.5));
         this.world = world;
         sprite = world.game.spriteLoader.load("player.png", new Vector2D(6,10), 1/8.0);
         input = world.game.input;
+        view = world.game.view;
     }
 
     @Override
@@ -20,7 +22,31 @@ public class Player extends Entity {
     public void tick(double deltaTime) {
         super.tick(deltaTime);
         if (input.isHeld(' ')) {
-            System.out.print("SPACE");
+            // if on ground (exactly 0 vertical velocity), jump
+            if (getVelocity().getY() == 0) {
+                setVelocity(new Vector2D(getVelocity().getX(), -20));
+            }
+            // 1/2 gravity while moving upwards & holding space - not *realistic* physics, but it feels better
+            if (getVelocity().getY() < 0) {
+                setVelocity(getVelocity().subtract(Physics.GRAVITY.multiply(deltaTime/2)));
+            }
+        }
+        int moveH = (input.isHeld('D') ? 1 : 0) - (input.isHeld('A') ? 1 : 0);
+        Vector2D v = getVelocity();
+        if (moveH == 0) {
+            setVelocity(new Vector2D(v.getX()*0.8, v.getY()));
+        } else {
+            setVelocity(new Vector2D(v.getX()+moveH, v.getY()));
+        }
+
+        // temporary controls for placing & breaking tiles
+        if (input.isHeld('Q')) {
+            TilePos pos = new TilePos(view.screenToWorldPos(input.getMousePos()));
+            world.setTile(pos, Tile.AIR);
+        }
+        if (input.isHeld('E')) {
+            TilePos pos = new TilePos(view.screenToWorldPos(input.getMousePos()));
+            world.setTile(pos, Tile.STONE);
         }
     }
 
