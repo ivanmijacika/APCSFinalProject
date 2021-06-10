a7452e25b7ebc7740cab1329eae7a87a8becbfd0
yelsharawy <yelsharawy20@stuy.edu>
2021-06-09 23:55:17 -0400

    Changed ItemStack.java formatting, added Inventory property

--- ItemStack.java
+++ ItemStack.java
@@ -1,28 +1,41 @@
-public class ItemStack{
-
-  private Item item;
-  private int count;
-
-  public ItemStack(Item item, int count){
-    this.item = item;
-    this.count = count;
-  }
-
-  public ItemStack(ItemStack other){
-    item = other.getItem();
-    count = other.getCount();
-  }
-
-  public Item getItem(){
-    return item;
-  }
-
-  public int getCount(){
-    return count;
-  }
-
-  public void setCount(int c){
-    count = c;
-  }
-
+public class ItemStack {
+	
+	private Item item;
+	private Inventory inventory;
+	private int count;
+	
+	public ItemStack(Item item, int count) {
+		this(item, count, null);
+	}
+	
+	public ItemStack(Item item, int count, Inventory inv) {
+		this.item = item;
+		this.count = count;
+		this.inventory = inv;
+	}
+	
+	public ItemStack(ItemStack other) {
+		this(other.getItem(), other.getCount());
+	}
+	
+	public Inventory getInventory() {
+		return inventory;
+	}
+	
+	public void setInventory(Inventory inv) {
+		inventory = inv;
+	}
+	
+	public Item getItem() {
+		return item;
+	}
+	
+	public int getCount() {
+		return count;
+	}
+	
+	public void setCount(int c) {
+		count = c;
+	}
+	
 }
