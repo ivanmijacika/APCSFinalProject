3424e0ebe94972365eea62fb779fcbb1a6b9321a
ivanmijacika <imijacika20@stuy.edu>
2021-06-10 22:17:47 -0400

    Implemented flipped() method

--- ISprite.java
+++ ISprite.java
@@ -8,5 +8,6 @@ public interface ISprite extends Cloneable {
     void draw(View view, Vector2D p);
     public ISprite withScale(double scale);
     void drawWithLight(View veiw, Vector2D p, double brightness);
+    public ISprite flipped();
 
 }
