90e128bf6c6d51e0bd66419ed0a9fc4a598c6666
yelsharawy <yelsharawy20@stuy.edu>
2021-06-10 23:28:09 -0400

    Added ItemEntity.java, representing an ItemStack dropped into the World

---
+++ ItemEntity.java
@@ -0,0 +1,26 @@
+public class ItemEntity extends Entity {
+	
+	private ItemStack stack;
+	private ISprite sprite;
+	
+	public ItemEntity(World world, Vector2D pos, ItemStack stack) {
+		super(world, pos, new Vector2D(10*Math.random()-5, -5), new Vector2D(0.5, 0.5));
+		this.stack = stack;
+		sprite = stack.getItem().getSprite().withScale(1/16.0);
+		sprite.setPivot(new Vector2D(4, 4));
+	}
+	
+	@Override
+	public void draw(double brightness) {
+		sprite.drawWithLight(getWorld().game.view, getPosition(), brightness);
+	}
+	
+	@Override
+	public void tick(double deltaTime) {
+		super.tick(deltaTime);
+		Vector2D vel = getVelocity();
+		
+		setVelocity(new Vector2D(vel.getX()*(1 - deltaTime), vel.getY()));
+	}
+	
+}
